491e163e3b328db2fd1b62d4723daadbab2d6977
Hugo-NF <hugonfonseca@hotmail.com>
2021-05-08 23:01:22 -0300

    🚧 Add threadable def guard

--- include/threadable.h
+++ include/threadable.h
@@ -1,8 +1,12 @@
+#ifndef THREADABLE_H
+#define THREADABLE_H
 
 class threadable {
     
 public:
     virtual void* run(void* thread_id) = 0;
-    virtual void load_from_json(const char* filename) = 0;
+    virtual int load_from_json(const char* filename) = 0;
     virtual void print_info() = 0;
 };
+
+#endif //THREADABLE_H
